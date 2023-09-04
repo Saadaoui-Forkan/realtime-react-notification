9052981ad966db9ba8a08a08b5b1127c668fdf67
unknown <forkan.saadaoui@gmail.com>
2023-09-04 21:33:57 +0100

    initial commit

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
 
-function Card() {
+const Card = () => {
+
   return (
-    <div>Card</div>
-  )
-}
+    <div className="card">
+      <div className="info">
+        <img src="https://images.pexels.com/photos/3497624/pexels-photo-3497624.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="userImg" />
+        <span>fullname</span>
+      </div>
+      <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="postImg" />
+      <div className="interaction">
+        <img src="https://images.pexels.com/photos/3497624/pexels-photo-3497624.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="cardIcon" />
+        <i className="fa-solid fa-heart cardIcon"></i>
+        <i className="fa-solid fa-comment cardIcon"></i>
+        <i className="fa-solid fa-share-nodes cardIcon"></i>
+        <i className="fa-solid fa-circle-info cardIcon infoIcon"></i>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
 
-function Navbar() {
+const Navbar = () => {
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <span className="logo">Lama App</span>
+      <div className="icons">
+        <div className="icon">
+          <i className="fa-solid fa-bell iconImg"></i>
+        </div>
+        <div className="icon" >
+          <i className="fa-solid fa-envelope iconImg"></i>
+        </div>
+        <div className="icon" >
+          <i className="fa-solid fa-gear iconImg"></i>
+        </div>
+      </div>
+        <div className="notifications">
+          <button className="nButton">
+            Mark as read
+          </button>
+        </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
